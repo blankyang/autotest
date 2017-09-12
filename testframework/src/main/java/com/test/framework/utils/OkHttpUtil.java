4811e353b303fc93a24787c57f9d2a9ac613a924
yangxiaobin <yangyangtbs@163.com>
2017-09-12 16:07:09 +0800

    123

--- testframework/src/main/java/com/test/framework/utils/OkHttpUtil.java
+++ testframework/src/main/java/com/test/framework/utils/OkHttpUtil.java
@@ -13,7 +13,8 @@ import okhttp3.Response;
 public class OkHttpUtil {
 
 	public static final MediaType IMG = MediaType.parse("image/png");
-	public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+	public static final MediaType JSON = MediaType
+			.parse("application/json; charset=utf-8");
 
 	public static String get(String url) {
 		try {
@@ -27,37 +28,11 @@ public class OkHttpUtil {
 		return "请求失败";
 	}
 
-	public static String post4Json(String url, String json) {
-		try {
-			OkHttpClient client = new OkHttpClient();
-			RequestBody body = RequestBody.create(JSON, json);
-			Request req = new Request.Builder().url(url).post(body).build();
-			Response response = client.newCall(req).execute();
-			return response.body().string();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return "请求失败";
-	}
-
-	public static String post4Img(String url, String json) {
-		try {
-			OkHttpClient client = new OkHttpClient();
-			RequestBody body = RequestBody.create(JSON, json);
-			Request req = new Request.Builder().url(url).post(body).build();
-			Response response = client.newCall(req).execute();
-			return response.body().string();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return "请求失败";
-	}
-
-	public static String post4Form(String url, Map<String,String> params) {
+	public static String post4Form(String url, Map<String, String> params) {
 		try {
 			OkHttpClient client = new OkHttpClient();
 			okhttp3.FormBody.Builder builder = new FormBody.Builder();
-			for(Map.Entry<String, String> en : params.entrySet()){
+			for (Map.Entry<String, String> en : params.entrySet()) {
 				builder.add(en.getKey(), en.getValue());
 			}
 			RequestBody body = builder.build();
@@ -69,7 +44,5 @@ public class OkHttpUtil {
 		}
 		return "请求失败";
 	}
-	
-	
 
 }
